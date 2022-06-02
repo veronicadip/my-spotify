a9a8f647ee8b968f4d4ca75d4a136e0b57e4898d
veronicadip <dipmveronica@gmail.com>
2022-06-02 12:48:32 -0300

    add home tests

--- src/routes/Home.test.tsx
+++ src/routes/Home.test.tsx
@@ -1,16 +1,52 @@
 import Home from "../routes/Home";
-import { SpotifyWebApi } from "spotify-web-api-ts";
-import renderWithRouter from "../testUtils"
+import { renderWithRouter } from "../testUtils";
 import { act } from "react-dom/test-utils";
+import SpotifyWebApi from "spotify-web-api-js";
 
+jest.mock("spotify-web-api-ts");
 
+describe("<Home>", function () {
+  const searchMock = jest.fn();
 
+  jest.spyOn(SpotifyWebApi.prototype, "search").mockReturnValue({
+    search: searchMock,
+  });
 
-it("renders without crashing", async () => {
+  beforeEach(() => {
+    searchMock.mockClear();
+  });
+
+  it("renders without crashing", async () => {
     await act(async () => {
-        jest.mock("spotify-web-api-ts", () => ({
+      await renderWithRouter(<Home />);
+    });
+  });
 
-        }))
-        await renderWithRouter(<Home />);
-    })
-})
+  it("renders an empty message when no results are found", async () => {
+    searchMock.mockResolvedValue({ albums: [], artists: [], tracks: [] });
+
+    await act(async () => {
+      await renderWithRouter(<Home />);
+      // TODO: check message when empty
+    });
+  });
+
+  it("renders an error message on errors", async () => {
+    searchMock.mockRejectedValue({});
+
+    await act(async () => {
+      await renderWithRouter(<Home />);
+      // TODO: check error message
+    });
+  });
+
+  it("renders results", async () => {
+    searchMock.mockResolvedValue({
+      albums: [],
+    });
+
+    await act(async () => {
+      await renderWithRouter(<Home />);
+    });
+  });
+});
--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,122 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
+import { Album } from "spotify-web-api-ts/types/types/SpotifyObjects";
 
-// const SpotifyWebApiMock: {
-//     setAccessToken: () => {},
-//     search: {
-//         search: () => {
-//             artists: {
-//                 items: [
-//                     {
-//                         images: [
-//                             { url: "https://i.scdn.co/image/ab67616100005174e9348cc01ff5d55971b22433" },
-//                             { url: "https://i.scdn.co/image/ab67616100005174e9348cc01ff5d55971b22433" },
-//                             { url: "https://i.scdn.co/image/ab67616100005174e9348cc01ff5d55971b22433" }
-//                         ],
-//                         name: "the beatles",
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     }
-//                 ]
-//             },
-//             albums: {
-//                 items: [
-//                     {
-//                         images: [
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" },
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" },
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" }
-//                         ],
-//                         name: "abbey road",
-//                         artists: [
-//                             {
-//                                 name: "the beatles"
-//                             }
-//                         ],
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     },
-//                     {
-//                         images: [
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" },
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" },
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" }
-//                         ],
-//                         name: "abbey road",
-//                         artists: [
-//                             {
-//                                 name: "the beatles"
-//                             }
-//                         ],
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     },
-//                     {
-//                         images: [
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" },
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" },
-//                             { url: "https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375" }
-//                         ],
-//                         name: "abbey road",
-//                         artists: [
-//                             {
-//                                 name: "the beatles"
-//                             }
-//                         ],
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     }
-//                 ]
-//             },
-//             tracks: {
-//                 items: [
-//                     {
-//                         album: {
-//                             images: [
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                             ]
-//                         },
-//                         name: "maxwell silver hammer",
-//                         artists: [
-//                             { name: "the beatles" }
-//                         ],
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     },
-//                     {
-//                         album: {
-//                             images: [
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                             ]
-//                         },
-//                         name: "abbey road",
-//                         artists: [
-//                             { name: "the beatles" }
-//                         ],
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     },
-//                     {
-//                         album: {
-//                             images: [
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                                 { url: "https://i.scdn.co/image/ab67616d00004851dc30583ba717007b00cceb25" },
-//                             ]
-//                         },
-//                         name: "abbey road",
-//                         artists: [
-//                             { name: "the beatles" }
-//                         ],
-//                         id: "6dGnYIeXmHdcikdzNNDMm2"
-//                     }
-//                 ]
-//             }
+export const renderWithRouter = async (
+  component: JSX.Element,
+  { route = "/" } = {}
+) => {
+  window.history.pushState({}, "Test page", route);
+  return render(component, { wrapper: BrowserRouter });
+};
 
-//         }
-//     }
-// }
-const renderWithRouter = async (component: JSX.Element, { route = "/" } = {}) => {
-    window.history.pushState({}, "Test page", route);
-    return render(component, { wrapper: BrowserRouter })
+const album = {
+  //....
+};
+
+const albums = [album];
+
+function mockAlbum(overrides: Partial<Album>): Partial<Album> {
+  return {
+    name: "",
+    ...overrides,
+  };
 }
 
-export default renderWithRouter;
+const albums2 = [mockAlbum({ name: "name1" }), mockAlbum({ name: "name2" })];
