b8a4dd84b965e12955c4d7289c5efb3007ba5340
Vero <vero@Robertos-Mac-mini-2.local>
2022-06-14 21:55:10 -0300

    add custom environment variable for access token

--- src/lib/accessToken.tsx
+++ src/lib/accessToken.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const currentAccessToken =
-  'BQCnVrJ_xe8AcbqOUlhenlbUROLNT3FCNuE9X_dJ5426e60e9AfwJxwWLb8nShHNNmgkcC1DV4jL4-0LTUEMszoxhU_z8j0xIHHvNGUEQsTnD384lsuL';
+const { REACT_APP_ACCESS_TOKEN } = process.env as any
+const currentAccessToken = REACT_APP_ACCESS_TOKEN
+
+
 
 export default currentAccessToken;
